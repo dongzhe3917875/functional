7fdb5951ce047faf2b73c4c664de34fa000d3e8e
dongzhe3917875 <dongzhe3917875@163.com>
2017-02-01 15:42:16 +0800

    add a quote

--- container/async.js
+++ container/async.js
@@ -6,4 +6,5 @@ var readFile = fileName => new Task((reject, result) => {
 	fs.readFile(fileName, ((err, data) => err ? reject(err) : result(data)))
 })
 // 例子中的 reject 和 result 函数分别是失败和成功的回调
+// 都是把对未来的操作的指示放在一个时间胶囊里
 console.log(readFile('io.js').fork(err => console.log(err), data => console.log(data)))
